2b9bc70605912d14e39b0267e4c167e2d8b947fb
SnehaMishra05 <s.mishra5623@gmail.com>
2026-03-04 21:01:45 +0530

    Cards Project

---
+++ 04-cards-project/src/components/Card.jsx
@@ -0,0 +1,55 @@
+import { Bookmark } from "lucide-react"
+const Card = (props) => {
+    // return (
+    // <div className="card">
+    //     <div>
+    //     <div className="top">
+    //         <img src="https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg" alt="" />
+    //         <button>Save <Bookmark size={12}/></button>
+    //     </div>
+    //     <div className="center">
+    //         <h3>Amazon <span>5 days ago</span></h3>
+    //         <h2>Senior UI/UX Designer</h2>
+    //         <div className="tag">
+    //             <h4>Part Time</h4>
+    //             <h4>Senior Level</h4>
+    //         </div>
+    //     </div>
+    //     </div>
+    //         <div className="bottom">
+    //             <div>
+    //                 <h3>$120/hr</h3>
+    //                 <p>Mumbai, India</p>
+    //             </div>
+    //             <button>Apply Now</button>
+    //         </div>
+    //     </div>
+    // )
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="" />
+                    <button>Save <Bookmark size={10}/></button>
+                </div>
+                <div className="center">
+                    <h3>{props.companyName} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className="tag">
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
